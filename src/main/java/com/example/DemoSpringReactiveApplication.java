e1e6e7517399696bab15eab29c361df53fe40a54
Toshiaki Maki <makingx@gmail.com>
2015-12-03 13:35:38 +0900

    Update along with latest version

--- src/main/java/com/example/DemoSpringReactiveApplication.java
+++ src/main/java/com/example/DemoSpringReactiveApplication.java
@@ -4,18 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.server.servlet31.HttpHandlerServlet;
+import org.springframework.http.server.reactive.ServletHttpHandlerAdapter;
 import org.springframework.web.reactive.DispatcherHandler;
 
 @SpringBootApplication
 public class DemoSpringReactiveApplication {
 
     @Bean
-    HttpHandlerServlet httpHandlerServlet(ApplicationContext context) {
+    ServletHttpHandlerAdapter httpHandlerServlet(ApplicationContext context) {
         DispatcherHandler dispatcherHandler = new DispatcherHandler();
         dispatcherHandler.setApplicationContext(context);
 
-        HttpHandlerServlet servlet = new HttpHandlerServlet();
+        ServletHttpHandlerAdapter servlet = new ServletHttpHandlerAdapter();
         servlet.setHandler(dispatcherHandler);
         return servlet;
     }
